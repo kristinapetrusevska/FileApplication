81fdddf300b9badf7f4c690e4356a4ea13f30c8f
kristinapetrusevska <kristina1996ku@gmail.com>
2020-01-21 15:06:21 +0100

    Add project files.

---
+++ FileApp.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace FileApplication
+{
+    partial class FileApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileExplorer = new System.Windows.Forms.Button();
+            this.listOfFiles = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numOfRepetitionsLabel = new System.Windows.Forms.Label();
+            this.textBoxForSelectedFile = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // openFileExplorer
+            // 
+            this.openFileExplorer.Location = new System.Drawing.Point(181, 24);
+            this.openFileExplorer.Name = "openFileExplorer";
+            this.openFileExplorer.Size = new System.Drawing.Size(102, 24);
+            this.openFileExplorer.TabIndex = 1;
+            this.openFileExplorer.Text = "File Explorer";
+            this.openFileExplorer.UseVisualStyleBackColor = true;
+            this.openFileExplorer.Click += new System.EventHandler(this.openFileExplorer_Click);
+            // 
+            // listOfFiles
+            // 
+            this.listOfFiles.FormattingEnabled = true;
+            this.listOfFiles.ItemHeight = 16;
+            this.listOfFiles.Location = new System.Drawing.Point(26, 114);
+            this.listOfFiles.Name = "listOfFiles";
+            this.listOfFiles.Size = new System.Drawing.Size(269, 228);
+            this.listOfFiles.TabIndex = 2;
+            this.listOfFiles.SelectedIndexChanged += new System.EventHandler(this.listOfFiles_SelectedIndexChanged);
+            this.listOfFiles.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listOfFiles_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter search string:";
+            // 
+            // numOfRepetitionsLabel
+            // 
+            this.numOfRepetitionsLabel.AutoSize = true;
+            this.numOfRepetitionsLabel.Location = new System.Drawing.Point(132, 360);
+            this.numOfRepetitionsLabel.Name = "numOfRepetitionsLabel";
+            this.numOfRepetitionsLabel.Size = new System.Drawing.Size(127, 17);
+            this.numOfRepetitionsLabel.TabIndex = 4;
+            this.numOfRepetitionsLabel.Text = "Num of repetitions:";
+            // 
+            // textBoxForSelectedFile
+            // 
+            this.textBoxForSelectedFile.Location = new System.Drawing.Point(398, 82);
+            this.textBoxForSelectedFile.Name = "textBoxForSelectedFile";
+            this.textBoxForSelectedFile.Size = new System.Drawing.Size(299, 260);
+            this.textBoxForSelectedFile.TabIndex = 6;
+            this.textBoxForSelectedFile.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "List of matching files:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(395, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Text in selected file:";
+            // 
+            // FileApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 426);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxForSelectedFile);
+            this.Controls.Add(this.numOfRepetitionsLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listOfFiles);
+            this.Controls.Add(this.openFileExplorer);
+            this.Controls.Add(this.textBox1);
+            this.Name = "FileApp";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button openFileExplorer;
+        private System.Windows.Forms.ListBox listOfFiles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label numOfRepetitionsLabel;
+        private System.Windows.Forms.RichTextBox textBoxForSelectedFile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FileApp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace FileApplication
+{
+    public partial class FileApp : Form
+    {
+        public FileApp()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileExplorer_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                listOfFiles.Items.Clear();
+                int numOfRepetitions = 0;
+                string textFormat = ".txt";
+                string searchString = textBox1.Text;
+                string[] files = Directory.GetFiles(FBD.SelectedPath, "*.*", SearchOption.AllDirectories);
+
+                foreach (string file in files)
+                {
+                    string textInFile = File.ReadAllText(file, Encoding.UTF8);
+
+                    if (file.Contains(textFormat) && textInFile.Contains(searchString))
+                    {
+                        int countOfRepetitions = Regex.Matches(textInFile, searchString).Count;
+
+                        listOfFiles.Items.Add(file);
+                        numOfRepetitions += countOfRepetitions;
+                    }
+
+                }
+                this.numOfRepetitionsLabel.Text = "Num of repetitions:" + numOfRepetitions;
+
+            }
+        }
+
+        private void listOfFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBoxForSelectedFile.Text = File.ReadAllText(listOfFiles.SelectedItem.ToString(), Encoding.UTF8);
+        }
+        
+
+        private void listOfFiles_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            string selectedFile = listOfFiles.SelectedItem.ToString();
+            Process.Start(selectedFile);
+        }
+    }
+}
